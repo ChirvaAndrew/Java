0c9f60f302a4c197f986da03fbef005ec9ab6387
Andrey Chirva <andrew0218@yandex.ru>
2022-10-07 15:02:27 +0300

    Comment could be here, but it's not.

--- Pr2/src/Main.java
+++ Pr2/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
         public static void main(String[] args) {
             ShapeTest test1 = new ShapeTest();
-            System.out.println("Hi");
+            System.out.println("Hello");
         }
 }
