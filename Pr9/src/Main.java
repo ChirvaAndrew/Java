207d2f65bf3d99e0de7db2221bec6ce626511d4e
andrey <smail2204973@gmail.com>
2022-11-02 13:54:49 +0300

    Comment could be here, but it isn't

--- Pr9/src/Main.java
+++ Pr9/src/Main.java
@@ -7,31 +7,62 @@ public class Main {
     public static void main(String[] args) {
         Team first = new Team("Милан");
         Team second = new Team("Мадрид");
+
+        //Окно
         JFrame box = new JFrame("Матч");
+
+
+        //Счёт
+        JLabel result = new JLabel("Result: " + first.getScore() + " X " + second.getScore());
+        result.setBounds(5, 60, 100, 100);
+
+        //Последняя забившая команда
+        JLabel last = new JLabel("Last Scorer: N/A");
+        last.setBounds(5, 80, 150, 100);
+
+        //Победитель
+        JLabel winner = new JLabel("Winner: DRAW");
+        winner.setBounds(5, 100, 150, 100);
+
+        //Кнопка первой команды
         JButton firstTeam = new JButton(first.getName());
         firstTeam.setBounds(20, 15, 85, 20);
-        firstTeam.addActionListener(e -> click(1, first, second, box));
+        firstTeam.addActionListener(e -> click(1, first, second, result, last, winner));
+
+        //Кнопка второй команды
         JButton secondTeam = new JButton(second.getName());
         secondTeam.setBounds(150, 15, 85, 20);
-        secondTeam.addActionListener(e -> click(2, first, second, box));
-        JLabel result = new JLabel(first.getScore() + " X " + second.getScore());
-        result.setBounds(60, 60, 100, 100);
+        secondTeam.addActionListener(e -> click(2, first, second, result, last, winner));
+
+
+        //Добавление элеметов в окно
         box.add(result);
         box.add(firstTeam);
         box.add(secondTeam);
+        box.add(last);
+        box.add(winner);
         box.setSize(300, 300);
         box.setLayout(null);
         box.setVisible(true);
     }
 
-    static void click(int a, Team first, Team second, JFrame box, ) {
+    static void click(int a, Team first, Team second, JLabel box, JLabel last, JLabel winner) {
         if (a == 1) {
             first.click();
+            last.setText("Last Scorer: " + first.getName());
         }
         if (a == 2) {
             second.click();
+            last.setText("Last Scorer: " + second.getName());
         }
-        result.
+        box.setText("Result: " + first.getScore() + " X " + second.getScore());
+        if (first.getScore() > second.getScore()) {
+            winner.setText("Winner: " + first.getName());
+        } else if (first.getScore() == second.getScore()) {
+            winner.setText("Winner: DRAW");
+        } else {
+            winner.setText("Winner: " + second.getName());
+        }
     }
 }
 
