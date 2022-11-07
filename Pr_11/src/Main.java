1e276e769bcf636cf0c68d5b23f318068f2a5f2d
Andrey Chirva <andrew0218@yandex.ru>
2022-11-07 20:34:36 +0300

    fix 11. Broke 5. :\

--- Pr_11/src/Main.java
+++ Pr_11/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Counter attemt = new Counter(0);
         //Генерация случайного числа
         int rnd = (int) (Math.random() * 20.);
-        System.out.println(rnd);
+        //System.out.println(rnd);
 
         //Окно
         JFrame box = new JFrame("игра-угадайка");
@@ -47,12 +47,11 @@ public class Main {
     }
 
     static void click(JTextArea enter, int rnd, Counter attemt, JLabel output, JLabel counter, JButton submit) {
-
         attemt.up();
-        System.out.println(enter.getText());
         if (!Objects.equals(enter.getText(), "") && Integer.parseInt(enter.getText()) == rnd) {
             output.setText("Верно!");
             submit.setVisible(false);
+            return;
         }
         if (!Objects.equals(enter.getText(), "") && Integer.parseInt(enter.getText()) > rnd) {
             output.setText("Нет, загаданное число меньше");
@@ -65,7 +64,7 @@ public class Main {
         }
         counter.setText("Счётчик попыток " + attemt.getAttemt() + "/3");
 
-        if (attemt.getAttemt() == 3 && Integer.parseInt(enter.getText()) != rnd)
+        if (attemt.getAttemt() == 3)
         {
             output.setBounds(0, 160, 2000, 30);
             output.setText("Нет. Вы исчерпали количество попыток.\n загаданное число: " + rnd);
